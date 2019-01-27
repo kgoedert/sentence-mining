d4de5042552fa42fc7076a7c9f6499f9dc58238c
Kelly Goedert <kelly@substractum.com.br>
2019-01-27 09:14:24 -0200

    french for two weeks

--- sentence_to_deck.py
+++ sentence_to_deck.py
@@ -8,7 +8,7 @@ import re
 
 
 url = "https://api.soundoftext.com"
-ANKI_URL = "http://10.1.1.134:8765"
+ANKI_URL = "http://192.168.0.24:8765"
 
 def main():
     parser = argparse.ArgumentParser("Sentence Mining")
